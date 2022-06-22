4ac297200d68944eaf72b6475cf92d15403fb25f
Moura-Leonardo <leonardomoura2004@gmail.com>
2022-06-22 17:29:01 -0300

    main

---
+++ tours.js
@@ -0,0 +1,7 @@
+var tours = {
+    img: ima
+    title: "teste",
+    description: "Mollit dolor mollit irure dolor nulla nisi mollit pariatur.",
+    dscription2: "Ipsum irure occaecat occaecat et Lorem. Incididunt cillum duis voluptate amet reprehenderit. Laborum aliquip occaecat proident nulla et ipsum excepteur. Tempor in labore aliqua et. Sunt ea mollit officia tempor incididunt. Sit cillum ex proident consectetur esse aliqua voluptate excepteur tempor. Amet deserunt laboris non magna duis do.",
+    id:01
+}
